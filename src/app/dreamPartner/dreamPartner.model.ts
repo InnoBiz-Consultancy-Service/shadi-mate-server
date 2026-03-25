be452f1e15aa96fc327d2fde6e247f5897c65845
Shanjida Jeem <shanjidajeem312@gmail.com>
2026-03-25 16:09:21 +0600

    feat: Add DreamPartner model, controller, and service for managing user preferences

---
+++ src/app/dreamPartner/dreamPartner.model.ts
@@ -0,0 +1,27 @@
+// models/dreamPartner.model.ts
+import { Schema, model } from "mongoose";
+import { EconomicalStatus, Habits } from "../modules/profile/profile.interface";
+
+const dreamPartnerPreferenceSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+
+    practiceLevel: { 
+        type: String, 
+        enum: ["Practicing", "Regular", "Occasional", "Not Practicing"], 
+        required: true 
+    },
+
+    economicalStatus: { 
+        type: String, 
+        enum: Object.values(EconomicalStatus), 
+        required: true 
+    },
+
+    habits: { 
+        type: [String], 
+        enum: Object.values(Habits), 
+        required: true 
+    },
+}, { timestamps: true });
+
+export const DreamPartnerPreference = model("DreamPartnerPreference", dreamPartnerPreferenceSchema);
